1b57bc9781c5a8e17cf2340f8a3b25e983aaf2bd
“Ali <tadayoni.ali.enayat@gmail.com>
2024-09-13 22:23:38 +0330

    clean up

--- src/js/views/recipeView.js
+++ src/js/views/recipeView.js
@@ -2,7 +2,6 @@ import View from "./View";
 
 import icons from "url:../../img/icons.svg";
 import { Fraction } from "fractional";
-
 class RecipeView extends View {
   _parentElement = document.querySelector(".recipe");
   _errorMessage = "We could not find tht recipe. please try another one!";
